1660cdc9cf3e124d2843c6a4208ef9357cd8f55a
gerald2009-ac <geraldac>
2026-07-08 13:43:17 +0100

    Intial commit

---
+++ javaScript/index.js
@@ -0,0 +1,34 @@
+function firstLink(params) {
+    debugger;
+    document.getElementById("mode").style.backgroundColor = "white";
+    document.getElementById("mode").style.color = "green";
+    document.getElementById("mode").style.border = "1px solid ghoswhite";
+
+    document.getElementById("dome").style.backgroundColor = "transparent";
+    document.getElementById("dome").style.color = "white";
+    document.getElementById("deme").style.border = "none"
+
+}
+function secondLink(params) {
+    debugger;
+    document.getElementById("dome").style.backgroundColor = "white";
+    document.getElementById("dome").style.color = "green";
+    document.getElementById("deme").style.border = "1px solid ghoswhite";
+    document.getElementById("emod").style.backgroundColor = "transparent";
+    document.getElementById("emod").style.color = "white";
+    document.getElementById("emod").style.border = "none"
+}
+function thirdLink(params) {
+    debugger;
+    document.getElementById("emod").style.backgroundColor = "white";
+    document.getElementById("emod").style.color = "green";
+    document.getElementById("emod").style.border = "1px solid ghoswhite";
+    document.getElementById("mode").style.backgroundColor = "transparent";
+    document.getElementById("mode").style.color = "white";
+    document.getElementById("mode").style.border = "none"
+}
+
+function studentAdmision(){
+    debugger
+    alert("Thank you for the application. We will reach out to you soon with details of your entrance examination");
+}
